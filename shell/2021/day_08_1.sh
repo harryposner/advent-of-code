d2209a04517360d7b824894ed6b1d7f3e0b9d2f0
Harry Posner <harryposner@users.noreply.github.com>
2021-12-17 17:02:08 -0800

    Cleaner awk patterns for day 8 part 1 in bash

--- shell/2021/day_08_1.sh
+++ shell/2021/day_08_1.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 sed 's/.*| //g' <&0 \
 | sed 's/ /\n/g' \
 | awk '
-	/^[a-g]{2}$/ { n1++ }
-	/^[a-g]{4}$/ { n4++ }
-	/^[a-g]{3}$/ { n7++ }
-	/^[a-g]{7}$/ { n8++ }
-	END          { print(n1 + n4 + n7 + n8) }'
+	/^[a-g]{2,4}$/ { acc++ }
+	/^[a-g]{7}$/   { acc++ }
+	END            { print(acc++) }'
