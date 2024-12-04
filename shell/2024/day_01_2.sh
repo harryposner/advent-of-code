1b94cd1be7f47b37d102f2e9ec2d55aa94df3118
Harry Posner <harryposner@users.noreply.github.com>
2024-12-04 14:45:57 -0500

    it's part 2

--- shell/2024/day_01_2.sh
+++ shell/2024/day_01_2.sh
@@ -4,24 +4,24 @@ set -euo pipefail
 
 INPUT=$(cat)
 
-touch /tmp/advent_of_code_2024_01_1
+touch /tmp/advent_of_code_2024_01_2
 
 sed 's/\s\+/:/g' <<< "$INPUT" \
 	| cut -d ':' -f 2 \
 	| sort \
 	| uniq -c \
 	| sed -e 's/^/freq /g' \
-	>> /tmp/advent_of_code_2024_01_1
+	>> /tmp/advent_of_code_2024_01_2
 
 sed 's/\s\+/:/g' <<< "$INPUT" \
 	| cut -d ':' -f 1 \
 	| sed -e 's/^/left /g' \
-	>> /tmp/advent_of_code_2024_01_1
+	>> /tmp/advent_of_code_2024_01_2
 
 awk '\
 	/^freq/ { freqs[$3] = $2 } \
 	/^left/ { result += $2 * freqs[$2] } \
 	END { print result }
-	' < /tmp/advent_of_code_2024_01_1
+	' < /tmp/advent_of_code_2024_01_2
 
-rm /tmp/advent_of_code_2024_01_1
+rm /tmp/advent_of_code_2024_01_2
