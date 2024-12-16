1571857843e59b4fa1805effb71acd9d5e6c81d0
Harry Posner <harryposner@users.noreply.github.com>
2024-12-16 10:07:33 -0800

    2024 day 9 in rust

--- rust/2024/src/bin/09.rs
+++ rust/2024/src/bin/09.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(9);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 enum Block {
     Free,
     File(u64),
@@ -42,12 +42,53 @@ impl Drive {
     fn checksum(&self) -> u64 {
         let mut result = 0;
         for (i, block) in self.blocks.iter().enumerate() {
-            match block {
-                Block::Free => break,
-                Block::File(id) => result += (i as u64) * id,
+            if let Block::File(id) = block {
+                result += (i as u64) * id
             }
         }
         result
+    }
+
+    fn move_chunk(&mut self, from: usize, to: usize, length: usize) {
+        for i in 0..length {
+            self.blocks.swap(from + i, to + i);
+        }
+    }
+
+    fn file_start(&self, end: usize) -> usize {
+        let mut i = end - 1;
+        while i > 0 && self.blocks[i - 1] == self.blocks[end - 1] {
+            i -= 1;
+        }
+        i
+    }
+
+    fn is_chunk_free(&self, start: usize, length: usize) -> bool {
+        self.blocks[start..start + length]
+            .iter()
+            .all(|block| *block == Block::Free)
+    }
+
+    fn move_files(&mut self) {
+        let mut file_end = self.blocks.len();
+        while file_end > 0 {
+            if self.blocks[file_end - 1] == Block::Free {
+                file_end -= 1;
+                continue;
+            }
+            let file_start = self.file_start(file_end);
+            let file_size = file_end - file_start;
+            if file_start < file_size {
+                break;
+            }
+            for free_start in 0..file_start {
+                if self.is_chunk_free(free_start, file_size) {
+                    self.move_chunk(file_start, free_start, file_size);
+                    break;
+                }
+            }
+            file_end = file_start;
+        }
     }
 }
 
@@ -58,7 +99,9 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut drive = Drive::from_input(input);
+    drive.move_files();
+    Some(drive.checksum())
 }
 
 #[cfg(test)]
@@ -74,6 +117,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2858));
     }
 }
